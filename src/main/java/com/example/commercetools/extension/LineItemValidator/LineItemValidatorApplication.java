79f2322e8a4053254337c34841ed41db7eba0fcb
Daniel Rusev <drusev@Daniels-MacBook-Pro.local>
2021-06-10 17:33:36 +0300

    First commit

---
+++ src/main/java/com/example/commercetools/extension/LineItemValidator/LineItemValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercetools.extension.LineItemValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineItemValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineItemValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/commercetools/extension/LineItemValidator/LineItemValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commercetools.extension.LineItemValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineItemValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
